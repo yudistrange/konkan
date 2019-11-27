a52ff65a6fceab0e296070a862c25f24d25e2e29
udit <yudistrange@gmail.com>
2019-11-27 07:54:10 +0530

    Extend protocol to read/write clojure maps to postgres json columns

---
+++ src/clj/konkan/db/core.clj
@@ -0,0 +1,29 @@
+(ns konkan.db.core
+  (:require [clojure.data.json :as json]
+            [next.jdbc.result-set :as result-set]
+            [next.jdbc.prepare :as prepare])
+  (:import java.sql.PreparedStatement
+           java.sql.Timestamp
+           org.postgresql.util.PGobject))
+
+(defn- value-to-json-pgobject [value]
+  (doto (PGobject.)
+    (.setType "json")
+    (.setValue (json/write-str value))))
+
+(defn- json-pgobject-to-value [^PGobject pgobject]
+  (-> pgobject
+      (.getValue)
+      (json/read-str :key-fn keyword)))
+
+(extend-protocol prepare/SettableParameter
+  clojure.lang.IPersistentMap
+  (set-parameter [^clojure.lang.IPersistentMap v ^PreparedStatement ps ^long i]
+    (.setObject ps i (value-to-json-pgobject v))))
+
+(extend-protocol result-set/ReadableColumn
+  org.postgresql.util.PGobject
+  (read-column-by-label ^clojure.lang.IPersistentMap [^org.postgresql.util.PGobject v _]
+    (json-pgobject-to-value v))
+  (read-column-by-index ^clojure.lang.IPersistentMap [^org.postgresql.util.PGobject v _2 _3]
+    (json-pgobject-to-value v)))
