db1b57d6428a1727858418b0fd1fc516a75e0560
udit <yudistrange@gmail.com>
2019-11-27 07:53:14 +0530

    Run migrations via lein run
    Migrate: lein run migrate
    Rollback: lein run rollback

--- src/clj/konkan/core.clj
+++ src/clj/konkan/core.clj
@@ -2,10 +2,17 @@
   (:require [konkan.web :as web]
             [konkan.config :as config]
             [konkan.db.pool :as db-pool]
-            [ring.adapter.jetty :as jetty]))
+            [konkan.db.migration :as db-migration]
+            [ring.adapter.jetty :as jetty]
+            [next.jdbc :as jdbc]
+            [konkan.language.db :as lang-db]))
 
 (defn -main
   [& args]
   (config/init!)
   (db-pool/init!)
-  (jetty/run-jetty web/handler (config/http-spec)))
+  (if-not (empty? args)
+    (case (first args)
+      "migrate"  (db-migration/migrate)
+      "rollback" (db-migration/rollback))
+    (jetty/run-jetty web/handler (config/http-spec))))
